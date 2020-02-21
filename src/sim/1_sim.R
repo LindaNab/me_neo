6ac626b184fb370f10767fe88039a01b3e64e2c6
Nab <L.Nab@lumc.nl>
2020-02-21 16:40:37 +0100

    add function to perform simulation

---
+++ src/sim/1_sim.R
@@ -0,0 +1,26 @@
+#############################################################
+## Internal validation sampling strategies for exposure 
+## measurement error correction
+##
+## 1 - Run simulation study
+## lindanab4@gmail.com - 20200221
+#############################################################
+
+##############################
+# 0 - Load librairies + source code 
+##############################
+source(file = "./src/dgm/0_sim_scen.R")
+source(file = "./src/dgm/1_gen_data.R")
+
+############################## 
+# 1 - Helper Functions ----
+##############################
+simulation <- function(S = 5000,
+                       scenario){
+  for(i in 1:S){
+    data <- gen_data(lambda = scenarios()[126, 'lambda'],
+                     tau = scenarios()[126, 'tau'],
+                     heteroscedastic = scenarios()[126, 'heteroscedastic'])
+    
+  }
+}
