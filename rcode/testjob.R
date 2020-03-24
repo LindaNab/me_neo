a18ce6c633109c22ab65181ccd80a218aec66877
Linda Nab <l.nab@lumc.nl>
2020-03-24 13:52:22 +0100

    add more sim scenarios

--- rcode/testjob.R
+++ rcode/testjob.R
@@ -15,11 +15,13 @@ source(file = "./rcode/sim/run_sim.R")
 # 1 - Run simulation study 
 ##############################
 # Select datagen_scenarios and analysis_scenarios to be used
-use_datagen_scenarios <- datagen_scenarios()[16,]
-use_analysis_scenarios <- analysis_scenarios()[c(46, 52),]
+#use_datagen_scenarios <- datagen_scenarios()[16,]
+#use_analysis_scenarios <- analysis_scenarios()[c(46, 52),]
+use_datagen_scenarios <- datagen_scenarios()
+use_analysis_scenarios <- analysis_scenarios()
 
 # not run
 # run_sim()
-run_sim(rep = 10, 
+run_sim(rep = 5, 
         use_datagen_scenarios = use_datagen_scenarios, 
         use_analysis_scenarios = use_analysis_scenarios)
