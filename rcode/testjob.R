7c7f40b086055fc4305598d0600f992f3cb9315d
Linda Nab <l.nab@lumc.nl>
2020-03-24 13:44:56 +0100

    test job sim study 1

--- rcode/testjob.R
+++ rcode/testjob.R
@@ -1,4 +1,25 @@
-# test run
+#############################################################
+## Internal validation sampling strategies for exposure 
+## measurement error correction
+##
+## Executive script
+## lindanab4@gmail.com - 20200305
+#############################################################
 
-message <- "Hello world"
-saveRDS(message, file = "./helloworld.Rds")
+##############################
+# 0 - Load librairies + source code 
+##############################
+source(file = "./rcode/sim/run_sim.R")
+
+##############################
+# 1 - Run simulation study 
+##############################
+# Select datagen_scenarios and analysis_scenarios to be used
+use_datagen_scenarios <- datagen_scenarios()[16,]
+use_analysis_scenarios <- analysis_scenarios()[c(46, 52),]
+
+# not run
+# run_sim()
+run_sim(rep = 10, 
+        use_datagen_scenarios = use_datagen_scenarios, 
+        use_analysis_scenarios = use_analysis_scenarios)
