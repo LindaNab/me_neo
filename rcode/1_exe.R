576312282371dcb28954226f448e7cea3a8b4b9f
Linda Nab <l.nab@lumc.nl>
2020-03-25 15:50:18 +0100

    change the way of removing sim output files

--- rcode/1_exe.R
+++ rcode/1_exe.R
@@ -10,22 +10,57 @@
 # 0 - Load librairies + source code 
 ##############################
 source(file = "./rcode/sim/run_sim.R")
-
-# not run
-# run_sim()
+source(file = "./rcode/sim/process_sim_output.R")
+source(file = "./rcode/sim/sum_processed_output.R")
+source(file = "./rcode/tools/remove_data.R")
 
 ##############################
 # 1 - Run simulation study 
 ##############################
-run_sim(rep = 10, 
-        use_datagen_scenarios = datagen_scenarios()[1:2,])
+# Select datagen_scenarios and analysis_scenarios to be used
+use_datagen_scenarios <- datagen_scenarios()[c(1),]
+use_analysis_scenarios <- analysis_scenarios()
 
-# if you ran the simulation study once, and want to run it again, remove 
-# directories in ./data/output
-dirs <- paste0("./data/output/size_valdata_", 
-               unique(analysis_scenarios()[['size_valdata']]) * 100) 
-for (i in 1:length(dirs)){
-  if(dir.exists(dirs[i])){
-    system(paste0("rm -r ", dirs[i]))
-  }
+# not run
+# run_sim()
+run_sim(rep = 10, 
+        use_datagen_scenarios = use_datagen_scenarios, 
+        use_analysis_scenarios = use_analysis_scenarios)
+
+##############################
+# 2 - Process simulation output 
+##############################
+# not run
+# process_sim_output()
+process_sim_output(use_datagen_scenarios = use_datagen_scenarios,
+                   use_analysis_scenarios = use_analysis_scenarios)
+
+##############################
+# 3 - Summarize processed simulation output 
+##############################
+# not run
+# summarize_sim()
+summarize_sim(use_datagen_scenarios = use_datagen_scenarios,
+              use_analysis_scenarios = use_analysis_scenarios)
+
+
+# Remove output and directories after test running the simulation
+remove_files(use_analysis_scenarios,
+             use_datagen_scenarios,
+             data_dir = "./data/output")
+remove_data_dirs(use_analysis_scenarios,
+                 data_dir = "./data/output")
+# Remove processed output and directories
+remove_files(use_analysis_scenarios,
+             use_datagen_scenarios,
+             data_dir = "./data/processed")
+remove_data_dirs(use_analysis_scenarios,
+                 data_dir = "./data/processed")
+# Remove summary
+summarised_dir <- "./data/summarised"
+file_name_summary <- paste0(summarised_dir, "/summary.Rds")
+if (file.exists(file_name_summary)){
+  system(paste0("rm ", file_name_summary))
+  print(paste0(file_name_summary, " removed!"))
 }
+
