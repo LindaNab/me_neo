aa59aab2445a65cd60113cadf3a7a32c721457ee
Linda Nab <l.nab@lumc.nl>
2020-04-07 22:41:07 +0200

    fix coding mistake in method

--- rcode/analyses/inadm_reg_cal.R
+++ rcode/analyses/inadm_reg_cal.R
@@ -6,17 +6,14 @@
 ## lindanab4@gmail.com - 20200303
 #############################################################
 inadm_reg_cal <- function(data){
-  # select subjects of whom VAT is measured (complete cases)
-  data_cc <- subset(data, in_valdata == 1)
   # fit calibration model in complete cases
   cal_mod <- lm(VAT ~ WC + TBF + age + sex,
-                data = data_cc, 
-                na.action = na.omit)
+                data = subset(data, in_valdata == 1))
   VAT_fitted <- predict.lm(cal_mod, newdata = data)
   # make copy of data that will be imputed
   data_imp <- data
   # impute fitted values if VAT is missing
-  data_imp$VAT[data$in_valdata == 1] <- VAT_fitted[data$in_valdata == 1]
+  data_imp$VAT[data$in_valdata == 0] <- VAT_fitted[data$in_valdata == 0]
   data_imp$in_valdata <- 1
   # perform complete case analysis on imputed data
   cc_fit <- complete_case(data_imp)
