f64dfe88c09a104631dfb34525738df101df07e6
Linda Nab <l.nab@lumc.nl>
2020-04-09 15:30:47 +0200

    Add tryCatch clause for solve

--- rcode/analyses/efficient_reg_cal.R
+++ rcode/analyses/efficient_reg_cal.R
@@ -11,8 +11,35 @@ efficient_reg_cal <- function(data){
   # fit from regression calibration
   rc_fit <- reg_cal(data)
   # efficient regression calibration (Spiegelman, 2001)
-  beta <- solve(solve(rc_fit$vcov) + solve(cc_fit$vcov)) %*%
-    (solve(rc_fit$vcov) %*% rc_fit$beta + solve(cc_fit$vcov) %*% cc_fit$beta)
-  vcov_beta <- solve(solve(rc_fit$vcov) + solve(cc_fit$vcov))
-  out <- list(beta = beta, vcov = vcov_beta)
+  beta_and_vcov <- calc_beta_and_vcov(rc_fit, cc_fit)
+  out <- list(beta = beta_and_vcov$beta, 
+              vcov = beta_and_vcov$vcov_beta)
 }
+
+calc_beta_and_vcov <- function(rc_fit, cc_fit) {
+  inv_vcov_rc_fit <- erc_solve(rc_fit$vcov)
+  inv_vcov_cc_fit <- solve(cc_fit$vcov)
+  if (all(is.na(inv_vcov_rc_fit))) {
+    beta <- c(NA, NA, NA, NA, NA)
+    vcov_beta <- matrix(NA, nrow = 5, ncol = 5)
+  } else {
+    beta <- solve(inv_vcov_rc_fit + inv_vcov_cc_fit) %*%
+      (inv_vcov_rc_fit %*% rc_fit$beta +
+         inv_vcov_cc_fit %*% cc_fit$beta)
+    vcov_beta <- solve(inv_vcov_rc_fit + inv_vcov_cc_fit)
+  }
+  return(list(beta = beta, vcov_beta = vcov_beta))
+}
+
+erc_solve <- function(vcov) {
+  out <- tryCatch(
+    {
+      solve(vcov)
+    },
+    error = function(cond){
+      message(cond)
+      return(matrix(NA, nrow = 5, ncol = 5))
+    }
+  )
+  return(out)
+} 
