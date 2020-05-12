891a15c4b456a283e4083f91a7974ace72585237
Linda Nab <l.nab@lumc.nl>
2020-05-12 23:27:18 +0200

    add code to get seeds from a file

--- rcode/exe/execute_simstudy_gha.R
+++ rcode/exe/execute_simstudy_gha.R
@@ -12,16 +12,24 @@
 source(file = "./rcode/sim/run_sim.R")
 args <- commandArgs(trailingOnly = TRUE)
 args <- as.numeric(args)
-# args <- scan(args[1])
-# print(input_from_file)
+# Select datagen_scenarios and analysis_scenarios to be used
+#args <- c(0, 5000)
+use_datagen_scenarios <- datagen_scenarios()[args[1] + 1, ] # row 1 is scen_num 0
+use_analysis_scenarios <- subset(analysis_scenarios(), sampling_strat == "uniform")
+# seeds
+source(file = "./rcode/tools/remove_data.R")
+analysis_scenario <- use_analysis_scenarios[1, ] # for each scen_num seeds are identical
+analysis_scenario$sampling_strat = "random"
+file <- seek_files_analysis_scenario(analysis_scenario,
+                                     use_datagen_scenarios,
+                                     data_dir = "./data/output")
+seeds <- readRDS(file)[,5]
 
 ##############################
 # 1 - Run simulation study 
 ##############################
-# Select datagen_scenarios and analysis_scenarios to be used
-use_datagen_scenarios <- datagen_scenarios()[args[1] + 1,] # row 1 is scen_num 0
-use_analysis_scenarios <- subset(analysis_scenarios(), sampling_strat == "uniform")
 # Run simulation study
 run_sim(rep = args[2],
         use_datagen_scenarios = use_datagen_scenarios,
-        use_analysis_scenarios = use_analysis_scenarios)
+        use_analysis_scenarios = use_analysis_scenarios,
+        seeds = seeds)
